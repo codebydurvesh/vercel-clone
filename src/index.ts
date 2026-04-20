efe84fc0e7d80910e55004a4902d8d21f00053ab
codebydurvesh <durvesh.gaikwad08@gmail.com>
2026-04-20 20:09:09 +0530

    get all the recuring files paths to store it in S3 later, as the folders cannot be directly uploaded to S3

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,28 @@ import express from "express";
 import cors from "cors";
 import { simpleGit } from "simple-git";
 import { generateId } from "./generateId.js";
+import path from "path";
+import { fileURLToPath } from "url";
+import { getAllFiles } from "./file.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.post("/deploy", async (req, res) => {
   const repoUrl = req.body.repoUrl;
   const id = generateId();
   //clone repoUrl using simple-git
-  await simpleGit().clone(repoUrl, `output/${id}`);
+  await simpleGit().clone(repoUrl, path.join(__dirname, `output/${id}`));
 
-  res.json({ message: "Deployment successful" });
+  const files = getAllFiles(path.join(__dirname, `output/${id}`));
+  console.log(files);
+  //put this in S3
+
+  res.json({ id: id, message: "Deployment successful" });
 });
 
 app.listen(3000, () => {
